baa98d53304407c613876c67a9430ce45e2a0305
long.nguyen <long.nguyen@uni-bielefeld.de>
2022-04-15 22:35:23 +0200

    Deduplicate geocoded sf before spatial join with NUTS (closes #2)

--- R/utils.R
+++ R/utils.R
@@ -17,11 +17,20 @@ osm_geometry <- function(location, nominatim_api) {
 }
 
 #' @importFrom dplyr .data
-nuts_join <- function(sf, nuts, return_geometry) {
-  ret <- sf::st_join(sf, st_buffer_1km(nuts), sf::st_covered_by) |>
+nuts_join <- function(geocoded_sf, nuts, return_geometry) {
+  distinct_sf <- geocoded_sf |>
+    dplyr::group_by(.data$place_id) |>
+    dplyr::slice(1) |>
+    dplyr::ungroup() |>
+    dplyr::select(.data$place_id)
+
+  distinct_joined <- sf::st_join(distinct_sf, st_buffer_1km(nuts), sf::st_covered_by) |>
     sf::st_drop_geometry() |>
-    dplyr::filter(!is.na(.data$nuts_1)) |>
-    nuts_slice_lowest() |>
+    nuts_slice_lowest()
+
+  ret <- geocoded_sf |>
+    sf::st_drop_geometry() |>
+    dplyr::left_join(distinct_joined, by = "place_id") |>
     dplyr::select(.data$location, .data$name, .data$nuts_1, .data$nuts_2, .data$nuts_3)
 
   if (isTRUE(return_geometry)) {
@@ -32,21 +41,17 @@ nuts_join <- function(sf, nuts, return_geometry) {
   ret
 }
 
-nuts_slice_lowest <- function(nuts_df) {
-  nuts_df |>
+nuts_slice_lowest <- function(locations_nuts) {
+  locations_nuts |>
     dplyr::mutate(
       nuts_level = purrr::pmap_int(
         list(.data$nuts_1, .data$nuts_2, .data$nuts_3),
         \(...) 3L - sum(is.na(c(...)))
-      ),
-      row_nr = dplyr::row_number()
+      )
     ) |>
-    dplyr::group_by(.data$location) |>
+    dplyr::group_by(.data$place_id) |>
     dplyr::slice_max(.data$nuts_level) |>
-    dplyr::ungroup() |>
-    dplyr::mutate(location = as.character(.data$location)) |>
-    dplyr::arrange(.data$row_nr) |>
-    dplyr::select(-.data$row_nr)
+    dplyr::ungroup()
 }
 
 st_buffer_1km <- function(sf) sf::st_buffer(sf, units::set_units(1000, m))
